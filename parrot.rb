97a83ad486082df00350c59af742945b74eb2b39
karen ercoli <karenercoli@gmail.com>
2018-11-17 13:00:43 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,4 @@
-def parrot(string = "Squawk!")
-  puts "#{string}"
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
 end# Create method `parrot` that outputs a given phrase and
 # returns the phrase
